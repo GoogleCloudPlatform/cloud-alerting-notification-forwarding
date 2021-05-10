d6194af266788e4ee20c429755e2704ba494fe7a
wdzc2002 <wdzc2002@gmail.com>
2021-05-10 15:31:31 +0000

    cleaned v1 trigger push

--- environments/wdzc-dev/outputs.tf
+++ environments/wdzc-dev/outputs.tf
@@ -20,7 +20,3 @@ output "topic" {
 output "url" {
   value = "${module.cloud_run_with_pubsub.url}"
 }
-
-output "pubsub_service_account" {
-  value = "${module.pubsub_service_account.service_account_email}"
-}
