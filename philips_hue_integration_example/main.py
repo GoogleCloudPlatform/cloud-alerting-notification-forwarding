32d37fa78a8255efb01b07150462ac60e6d6f205
wdzc2002 <wdzc2002@gmail.com>
2021-05-11 03:00:57 +0000

    gchat v1

--- philips_hue_integration_example/main.py
+++ philips_hue_integration_example/main.py
@@ -27,7 +27,8 @@ import json
 from flask import Flask, request
 
 import config
-from utilities import pubsub, philips_hue
+from httplib2 import Http
+from utilities import pubsub, message_format
 
 
 app_config = config.load()
@@ -45,6 +46,8 @@ app.config.from_object(app_config)
 @app.route('/', methods=['POST'])
 def handle_pubsub_message():
     pubsub_received_message = request.get_json()
+    pubsub_received_message = json.dumps(pubsub_received_message).replace('None', '""')
+    pubsub_received_message = json.loads(pubsub_received_message)
 
     # parse the Pub/Sub data
     try:
@@ -53,14 +56,7 @@ def handle_pubsub_message():
         logger.error(e)
         return (str(e), 400)
 
-    # load the notification from the data
-    try:
-        monitoring_notification_dict = json.loads(pubsub_data_string)
-    except json.JSONDecodeError as e:
-        logger.error(e)
-        return (f'Notification could not be decoded due to the following exception: {e}', 400)
-
-    return send_monitoring_notification_to_third_party(monitoring_notification_dict)
+    return send_monitoring_notification_to_third_party(pubsub_data_string)
 # [END run_pubsub_handler]
 
 
@@ -68,20 +64,39 @@ def send_monitoring_notification_to_third_party(notification):
     """Send a given monitoring notification to a third party service.
 
     Args:
-        notification: The dictionary containing the notification data.
+        notification: A dictionary with the parsed out pubsub notificaiton message.
 
     Returns:
         A tuple containing an HTTP response message and HTTP status code
         indicating whether or not sending the notification to the third
         party service was successful.
     """
-    logger.info('get a message: %s', json.dumps(notification))
-    return ('wdzc', 200)
 
+    # url is the Incoming webhooks url for a gchat room
+    
+    url = 'https://chat.googleapis.com/v1/spaces/AAAAHgCPlz4/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=swcIuT3X3Cjp3jzkP8tr3Vao24l_0EyODjvOod2_f_U%3D'
+
+    messages_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    # notification = message_format.parse_message(notification)
+    bot_message = {'text': notification}
+
+    http_obj = Http()
+
+    try:
+        response = http_obj.request(
+            uri = url,
+            method = 'POST',
+            headers = messages_headers,
+            body = json.dumps(bot_message, indent=4),
+        )
+        print(response)
+    except Exception as e:
+        return(str(e), 400)
+    return(notification, 200)
 
 if __name__ == '__main__':
     PORT = int(os.getenv('PORT')) if os.getenv('PORT') else 8080
-
     # This is used when running locally. Gunicorn is used to run the
     # application on Cloud Run. See entrypoint in Dockerfile.
     app.run(host='127.0.0.1', port=PORT, debug=True)
---
+++ philips_hue_integration_example/utilities/message_format.py
@@ -0,0 +1,92 @@
+# Copyright 2020 Google, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module to re-format Pub/Sub notifications to an easier to read format.
+
+This module defines functions and errors to handle input from Google Monitoring,
+such as Pub/Sub notifications.
+"""
+
+class Error(Exception):
+    """Base class for all errors raised in this module."""
+
+def check_data(data):
+    if data is None:
+        return ''
+    else:
+        return data
+
+def parse_message(pubsub_message):
+    incident_id = pubsub_message['incident']['incident_id']
+    resource_id = pubsub_message['incident']['resource_id']
+    resource_name = pubsub_message['incident']['resource_name']
+    resource_type = pubsub_message['incident']['resource']['type']
+    labels_instance_id = pubsub_message['incident']['resource']['labels']['instance_id']
+    labels_project_id = pubsub_message['incident']['resource']['labels']['project_id']
+    labels_zone = pubsub_message['incident']['resource']['labels']['zone']
+    resource_display_name = pubsub_message['incident']['resource_display_name']
+    resource_type_display_name = pubsub_message['incident']['resource_type_display_name']
+    metric_type = pubsub_message['incident']['metric']['type']
+    metric_displayName = pubsub_message['incident']['metric']['displayName']
+    started_time = pubsub_message['incident']['started_at']
+    policy_name = pubsub_message['incident']['policy_name']
+    condition_name = pubsub_message['incident']['condition_name']
+    name_condition = pubsub_message['incident']['condition']['name']
+    condition_displayName = pubsub_message['incident']['condition']['displayName']
+    condition_filter = pubsub_message['incident']['condition']['conditionThreshold']['filter']
+    condition_aggregrations = pubsub_message['incident']['condition']['conditionThreshold']['aggregations'][0]['alignmentPeriod']
+    condition_perSeriesAligner = pubsub_message['incident']['condition']['conditionThreshold']['aggregations'][0]['perSeriesAligner']
+    condition_comparison = pubsub_message['incident']['condition']['conditionThreshold']['comparison']
+    condition_duration = pubsub_message['incident']['condition']['conditionThreshold']['duration']
+    condition_trigger_count = pubsub_message['incident']['condition']['conditionThreshold']['trigger']
+    incident_url = pubsub_message['incident']['url']
+    incident_state = pubsub_message['incident']['state']
+    incident_ended_at = pubsub_message['incident']['ended_at']
+    incident_summary = pubsub_message['incident']['summary']
+
+    formatted_message = ('incident_id: ' + incident_id + '\n'
+        'resource_id: ' + resource_id + '\n'
+        'resource_name: ' + resource_name + '\n'
+        'resource: ' + '\n'
+        '\ttype: ' + resource_type + '\n'
+        '\tlabels: ' + '\n'
+        '\t\tinstance_id: ' + labels_instance_id + '\n'
+        '\t\tproject_id: ' + labels_project_id + '\n'
+        '\t\tzone: ' + labels_zone + '\n'
+        'resource_display_name: ' + resource_display_name + '\n'
+        'resource_type_display_name: ' + resource_type_display_name + '\n'
+        'metric: ' + '\n'
+        '\ttype: ' + metric_type + '\n'
+        '\tdisplayName: ' + metric_displayName + '\n'
+        'started_at: ' + str(started_time) + '\n'
+        'policy_name: ' + policy_name + '\n'
+        'condition_name: ' + condition_name + '\n'
+        'condition:' + '\n'
+        '\tname: ' + name_condition + '\n'
+        '\tdisplayName: ' + condition_displayName + '\n'
+        '\tconditionThreshold: ' + '\n'
+        '\t\tfilter: ' + condition_filter + '\n'
+        '\t\taggregrations: ' + '\n'
+        '\t\t\talignmentPeriod: ' + condition_aggregrations + '\n'
+        '\t\t\tperSeriesAligner: ' + condition_perSeriesAligner + '\n'
+        '\t\tcomparison: ' + condition_comparison + '\n'
+        '\t\tduration: ' + condition_duration + '\n'
+        '\t\ttrigger: ' + '\n'
+        '\t\t\tcount: ' + str(condition_trigger_count) + '\n'
+        'url: ' + incident_url + '\n'
+        'state: ' + incident_state + '\n'
+        'ended_at: ' + incident_ended_at + '\n'
+        'summary: ' + incident_summary + '\n')
+    
+    return formatted_message
